21db2d7f2ed6669888a4f44f4552c9d6bbd1a0a5
yutaoluo <yutaoluo@tencent.com>
2021-04-26 14:52:40 +0800

    add unit test.

--- examples_test.go
+++ examples_test.go
@@ -1,52 +1,12 @@
 package jsonassert_test
 
 import (
-	"fmt"
-	"testing"
-
 	"github.com/545080079/jsonassert"
+	"testing"
 )
 
-type printer struct{}
 
-func (p *printer) Errorf(format string, args ...interface{}) {
-	fmt.Println(fmt.Sprintf(format, args...))
-}
-
-// using the varible name 't' to mimic a *testing.T variable
-var t *printer
-
-func ExampleNew() {
-	ja := jsonassert.New(t)
-	ja.Assertf(true, `{"hello":"world"}`, `
-		{
-			"hello": "world"
-		}`)
-}
-
-func ExampleAsserter_Assertf_formatArguments() {
-	ja := jsonassert.New(t)
-	ja.Assertf(true, `{"hello":"世界"}`, `
-		{
-			"hello": "%s"
-		}`, "world")
-	//output:
-	//expected string at '$.hello' to be 'world' but was '世界'
-}
-
-func ExampleAsserter_Assertf_presenceOnly() {
-	ja := jsonassert.New(t)
-	ja.Assertf(true, `{"asdf":"not the right key name"}`, `
-		{
-			"asdf": "<<PRESENCE>>"
-		}`)
-	//output:
-	//unexpected object key(s) ["hi"] found at '$'
-	//expected object key(s) ["hello"] missing at '$'
-}
-
-
-func TestX(t *testing.T) {
+func TestJsonValuate1(t *testing.T) {
 
 	ja := jsonassert.New(t)
 	ja.Assertf(false, `{"b": 25}`, `
